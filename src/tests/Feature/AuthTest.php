718c04faf3f21e70ea80c91a98128bcaa6b87245
YUKKE <gossip.girl.y1009@gmail.co>
2026-03-05 21:59:36 +0900

    単体テスト下準備

--- src/tests/Feature/AuthTest.php
+++ src/tests/Feature/AuthTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use RefreshDatabase;
 
-class ExampleTest extends TestCase
+class AuthTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
